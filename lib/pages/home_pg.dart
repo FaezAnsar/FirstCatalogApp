a9cd840e2fa5fc834a84b35d6b88c9877f83b308
FaezAnsar <FAEZANSARAHMED2004@GMAIL.COM>
2023-09-19 11:31:13 +0500

    Day12 completed

--- lib/pages/home_pg.dart
+++ lib/pages/home_pg.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hindi_course/models/catalog.dart';
 import 'package:hindi_course/widgets/drawer.dart';
+import 'package:hindi_course/widgets/item_widget.dart';
 
 
 class HomePage extends StatelessWidget {
   
   final int days =30;
   final String name = "Codepur";
-  const HomePage({super.key});
+   HomePage({super.key});
+  final dummy_list = List.generate(15, (index) => CatalogModel.items[0]) ;
+
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +18,15 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Catalog App "),
       ),
-      body: Center(
-        child:Container(
-        child: Text("Welcome to $days days of fluter by $name"),
-        )
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        //builder renders items only on screen and some before and after it so when we scroll we don't feel they are being rendered after coming on screen
+        child: ListView.builder(
+          itemCount:dummy_list.length ,
+        itemBuilder: (context, index) {
+          return ItemWidget(
+            item: dummy_list[index]);
+        },),
       ),
       drawer: MyDrawer(),
     );
